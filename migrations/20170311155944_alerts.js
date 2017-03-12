391b57cd9eb507bc680c33963b1b1cac1a3f3bf5
Jamieson Bates <jamiesonbates@gmail.com>
2017-03-11 16:02:35 -0800

    Make alerts migration

---
+++ migrations/20170311155944_alerts.js
@@ -0,0 +1,26 @@
+'use strict';
+
+exports.up = function(knex) {
+  return knex.schema.createTable('alerts', (table) => {
+    table.increments();
+    table
+      .integer('user_id')
+      .references('id')
+      .inTable('users')
+      .notNullable()
+      .indeX();
+    table
+      .integer('offense_type_id')
+      .references('id')
+      .inTable('offense_types')
+      .notNullable()
+      .index();
+    table
+      .integer('range')
+      .notNullable()
+  });
+};
+
+exports.down = function(knex) {
+  return knex.schema.dropTable('alerts');
+};
