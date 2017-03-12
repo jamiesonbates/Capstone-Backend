db541e6144b3d06f927fbe1d4aaf0cab1566e50d
Jamieson Bates <jamiesonbates@gmail.com>
2017-03-12 15:47:50 -0700

    Build first two functions - next needs database seeding

--- bots/updateDatabase.js
+++ bots/updateDatabase.js
@@ -5,6 +5,8 @@ const { camelizeKeys, decamelizeKeys } = require('humps');
 const knex = require('../knex');
 const moment = require('moment');
 
+const crimeDictionary = require('../tests/testdata/crimeDictionary');
+
 const getPoliceReports = function() {
   const url = `https://data.seattle.gov/resource/y7pv-r3kh.json?$where=date_reported > '2016-10-24T11:19:00.000'`;
   return axios.get(url)
@@ -16,12 +18,20 @@ const getPoliceReports = function() {
     });
 };
 
-const filterReportsWithDictionary = function() {
+const filterReportsWithDictionary = function(reports) {
+  const filteredReports = reports.filter(report => {
+    for (const offense of crimeDictionary) {
+      if (offense.summarizedOffenseType === report.summarized_offense_description) {
+        return report;
+      }
+    }
+  })
 
+  return filteredReports;
 }
 
 const getMatchingData = function() {
-
+  
 }
 
 const identifyNewData = function() {
@@ -43,13 +53,17 @@ const updateData = function() {
 const runDatabaseJob = function() {
   return getPoliceReports()
     .then((data) => {
-      console.log(data[0]);
-    });
+      console.log(data.length);
+
+      return filterReportsWithDictionary(data);
+    })
+    .then((data) => {
+      console.log(data);
+    })
 
 
 }
 
 module.exports = {
-  getPoliceReports,
   runDatabaseJob
 }
