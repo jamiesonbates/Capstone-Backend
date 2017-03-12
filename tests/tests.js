ed2d30bd44cf49530c26fe6cddd31fe89f468447
Jamieson Bates <jamiesonbates@gmail.com>
2017-03-12 14:00:03 -0700

    Add preliminary file structure

---
+++ tests/tests.js
@@ -0,0 +1,12 @@
+'use strict';
+
+const assert = require('chai').assert;
+const knex = require('../knex');
+const nock = require('nock');
+const { suite, test } = require('mocha');
+
+suite('getPoliceReports function', () => {
+  test('')
+});
+
+suite('')
