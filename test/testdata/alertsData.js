3975660eaaeb4a98d848625b05be77ae2c9a7ddf
Jamieson Bates <jamiesonbates@gmail.com>
2017-03-14 17:49:59 -0700

    Create seed files for users and alerts

---
+++ test/testdata/alertsData.js
@@ -0,0 +1,78 @@
+'use strict';
+
+const alertsData = [
+  {
+    id: 1,
+    user_id: 1,
+    offense_type_id: 1,
+    range: 5000
+  },
+  {
+    id: 2,
+    user_id: 1,
+    offense_type_id: 2,
+    range: 4000
+  },
+  {
+    id: 3,
+    user_id: 1,
+    offense_type_id: 3,
+    range: 3000
+  },
+  {
+    id: 4,
+    user_id: 1,
+    offense_type_id: 4,
+    range: 3500
+  },
+  {
+    id: 5,
+    user_id: 1,
+    offense_type_id: 5,
+    range: 4500
+  },
+  {
+    id: 6,
+    user_id: 1,
+    offense_type_id: 6,
+    range: 5500
+  },
+  {
+    id: 7,
+    user_id: 1,
+    offense_type_id: 7,
+    range: 6000
+  },
+  {
+    id: 8,
+    user_id: 1,
+    offense_type_id: 8,
+    range: 6500
+  },
+  {
+    id: 9,
+    user_id: 1,
+    offense_type_id: 9,
+    range: 7000
+  },
+  {
+    id: 10,
+    user_id: 1,
+    offense_type_id: 10,
+    range: 3250
+  },
+  {
+    id: 11,
+    user_id: 1,
+    offense_type_id: 11,
+    range: 4250
+  },
+  {
+    id: 12,
+    user_id: 1,
+    offense_type_id: 12,
+    range: 4750
+  }
+];
+
+module.exports = alertsData;
---
+++ test/testdata/usersData.js
@@ -0,0 +1,13 @@
+'use strict';
+
+const usersData = [
+  {
+    id: 1,
+    email: 'jamiesonbates@gmail.com',
+    username: 'jamiesonbates',
+    home_lng: '-122.326560',
+    home_lat: '47.617756'
+  }
+];
+
+module.exports = usersData;
