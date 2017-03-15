ef53e4ae5a3df5b3f338f4402a4dcff867371c44
Jamieson Bates <jamiesonbates@gmail.com>
2017-03-14 17:39:51 -0700

    #15 Setup alert function structure

--- bots/sendAlerts.js
+++ bots/sendAlerts.js
@@ -1,3 +1,19 @@
 'use strict';
 
 // Dependencies
+const knex require('../knex');
+
+// Get all alerts + matching user from DB
+const getAllAlerts = function() {
+  
+}
+
+// For each alert, check whether new data is within the alert's given radius
+const checkForMatches = function() {
+
+}
+
+// Send alerts that matched with new data
+const sendAlerts = function() {
+
+}
