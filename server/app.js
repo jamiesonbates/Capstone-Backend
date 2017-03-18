acd3c2f6dbe03a419201b71fe30cb7de3ffc4cef
Jamieson Bates <jamiesonbates@gmail.com>
2017-03-18 13:15:23 -0700

    Attempt to fix cors issue

--- server/app.js
+++ server/app.js
@@ -10,6 +10,12 @@ const app = express();
 
 app.use(bodyParser.json());
 
+app.use((req, res, next) => {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+  next();
+});
+
 app.use('/api', require('./routes/api'));
 
 module.exports = app;
