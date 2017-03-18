c0da07c803f52d47f7ddabc1c1969feaeddc45c4
Jamieson Bates <jamiesonbates@gmail.com>
2017-03-18 16:57:01 -0700

    Add routes for authenticating a user

--- server/app.js
+++ server/app.js
@@ -3,12 +3,18 @@
 const express = require('express');
 const path = require('path');
 const port = process.env.PORT || 3000;
+const cookieParser = require('cookie-parser');
 
 const bodyParser = require('body-parser');
 
 const app = express();
 
+if (process.env.NODE_ENV !== 'production') {
+  require('dotenv').config();
+}
+
 app.use(bodyParser.json());
+app.use(cookieParser());
 
 app.use((req, res, next) => {
   res.header("Access-Control-Allow-Origin", "*");
--- server/routes/api.js
+++ server/routes/api.js
@@ -3,6 +3,9 @@
 const knex = require('../../knex');
 const router = require('express').Router();
 const bcrypt = require('bcrypt-as-promised');
+const boom = require('boom');
+const jwt = require('jsonwebtoken');
+const { camelizeKeys, decamelizeKeys } = require('humps');
 
 const {
   sendAlertsJob,
@@ -51,31 +54,116 @@ router.get('/runjob', (req, res, next) => {
 });
 
 router.post('/users', (req, res, next) => {
-  const { email, username, password, home_lat, home_lng} = req.body;
-  console.log(req.body);
+  const { email, username, password, home_lat, home_lng } = req.body;
 
-  bcrypt.hash(req.body.password, 12)
-    .then((hashed_password) => {
-      return knex('users').insert({
-        email,
-        username,
-        hashed_password,
-        home_lat: home_lat,
-        home_lng: home_lng
-      }, '*')
+  if (!email || !email.trim()) {
+    return next(boom.create(400, 'Email must not be blank'));
+  }
+
+  if (!password || password.length < 8) {
+    return next(boom.create(400, 'Password must be at least 8 characters long'));
+  }
+
+  knex('users')
+    .where('email', email)
+    .first()
+    .then((existingUser) => {
+      if (existingUser) {
+        return next(boom.create(400, 'Email already exists'));
+      }
+
+      return bcrypt.hash(password, 12);
+    })
+    .then((hashedPassword) => {
+      return knex('users')
+        .insert(decamelizeKeys({ email, username, hashedPassword, home_lat, home_lng}), '*');
     })
     .then((users) => {
-      console.log('users', users[0]);
-      const user = users[0];
-      console.log(user);
+      const user = camelizeKeys(users[0]);
 
-      delete user.hashed_password;
+      const claim = { userId: user.id };
+      const token = jwt.sign(claim, process.env.JWT_KEY, {
+        expiresIn: '500 days'
+      });
+
+      console.log('here');
+      res.cookie('token', token, {
+        httpOnly: true,
+        expires: new Date(Date.now() + 1000 * 60 * 60 * 24 * 500),
+        secure: router.get('env') === 'production'
+      });
+
+      delete user.hashedPassword;
 
       res.send(user);
     })
     .catch((err) => {
       next(err);
     });
+});
+
+router.get('/token', (req, res) => {
+  jwt.verify(req.cookies.token, process.env.JWT_KEY, (err, _claim) => {
+    if (err) {
+      return res.send(false);
+    }
+
+    res.send(true);
+  });
+});
+
+router.post('/token', (req, res, next) => {
+  let user;
+
+  const { email, password } = req.body;
+
+  if (!email || !email.trim()) {
+    return next(boom.create(400, 'Email must not be blank'));
+  }
+
+  if (!password || !password.trim()) {
+    return next(boom.create(400, 'Password must not be blank'));
+  }
+
+  knex('users')
+    .where('email', email)
+    .first()
+    .then((row) => {
+      if (!row) {
+        throw boom.create(400, 'Bad email or password');
+      }
+
+      user = camelizeKeys(row);
+
+      return bcrypt.compare(password, user.hashedPassword);
+    })
+    .then(() => {
+      const claim = { userId: user.id };
+      const token = jwt.sign(claim, process.env.JWT_KEY, {
+        expiresIn: '7 days'
+      });
+
+      res.cookie('token', token, {
+        httpOnly: true,
+        expires: new Date(Date.now() + 1000 * 60 * 60 * 24 * 7),
+        secure: router.get('env') === 'production'
+      });
+
+      delete user.hashedPassword;
+
+      res.send(user);
+    })
+    .catch(bcrypt.MISMATCH_ERROR, () => {
+      throw boom.create(400, 'Bad email or password');
+    })
+    .catch((err) => {
+      next(err);
+    });
+});
+
+router.delete('/token', (req, res) => {
+  res.clearCookie('token');
+  res.send(false);
 });
 
 module.exports = router;
